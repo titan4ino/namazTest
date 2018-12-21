1991099b4439475cc03320027ea0422da236062c
Tynystan <4ino.dev@gmail.com>
2018-12-21 15:45:25 +0600

    Initial commit

---
+++ app/src/main/java/kg/com/namaz/namaztest/clientServ/GitHubRepo.java
@@ -0,0 +1,17 @@
+package kg.com.namaz.namaztest.clientServ;
+
+public class GitHubRepo {
+    private int id;
+    private String name;
+
+    public GitHubRepo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
